e2eacd93f08498171444eef7125c9f9b649ccbde
yihui <yihui@gmail.com>
2022-06-25 09:38:59 +0700

    swagger说明优化

--- user-server/src/main/java/com/baisha/userserver/model/vo/user/UserSearchVO.java
+++ user-server/src/main/java/com/baisha/userserver/model/vo/user/UserSearchVO.java
@@ -13,13 +13,13 @@ import javax.persistence.Column;
 @ApiModel(value = "会员中心-查询请求对象")
 public class UserSearchVO {
 
-    @ApiModelProperty(value = "用户名 (長度3-20,只能輸入字母,數字,_)")
+    @ApiModelProperty(value = "用户名 (長度3-20,只能輸入字母,數字,_)(非TG用户必传)")
     String userName;
 
-    @ApiModelProperty(value = "TG用户ID")
+    @ApiModelProperty(value = "TG用户ID(TG用户查询必传)")
     private String tgUserId;
 
-    @ApiModelProperty(value = "TG群ID")
+    @ApiModelProperty(value = "TG群ID(TG用户查询必传)")
     private String tgGroupId;
 
 }
