d12bad4ad952742ad4196316da05428b934e2729
yihui <yihui@gmail.com>
2022-06-25 21:28:57 +0700

    优化时间

--- casino/casino-backend/src/main/java/com/baisha/backendserver/model/bo/BaseBO.java
+++ casino/casino-backend/src/main/java/com/baisha/backendserver/model/bo/BaseBO.java
@@ -1,5 +1,6 @@
 package com.baisha.backendserver.model.bo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -12,10 +13,12 @@ public class BaseBO {
 
     @ApiModelProperty(value = "ID")
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @ApiModelProperty(value = "创建时间")
     private Date createTime;
     @ApiModelProperty(value = "创建人")
     private String createBy;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @ApiModelProperty(value = "更新时间")
     private Date updateTime;
     @ApiModelProperty(value = "更新人")
