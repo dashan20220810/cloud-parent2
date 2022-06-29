845361865e23e846d31d89b30f9b8bf2e6fffddf
alvin <>
2022-06-29 16:33:50 +0700

    优化

--- casino/casino-web/src/main/java/com/baisha/casinoweb/controller/OrderController.java
+++ casino/casino-web/src/main/java/com/baisha/casinoweb/controller/OrderController.java
@@ -62,16 +62,13 @@ public class OrderController {
     	if ( BetVO.checkRequest(betVO)==false 
     			|| (isTgRequest && betVO.getTgChatId()==null) ) {
     		log.info("[下注] 检核失败");
-    		return ResponseUtil.fail();
+            return ResponseUtil.custom("命令错误，请参考下注规则");
     	}
     	
-    	// TODO 以table id查deskcode，deskcode查局号
-    	JSONObject deskJson = deskBusiness.queryDeskById(betVO.getTableId());
-    	if ( deskJson==null ) {
-    		log.warn("[下注] 桌台号查无资料, table id: {}", betVO.getTableId());
-            return ResponseUtil.custom("桌台资料错误");
+    	if ( betVO.getMinAmount()==null || betVO.getMaxAmount()==null || betVO.getMaxShoeAmount()==null ) {
+    		log.info("[下注] 检核失败");
+            return ResponseUtil.custom("命令错误，请参考下注规则");
     	}
-    	GameInfo gameInfo = gameInfoBusiness.getGameInfo(deskJson.getString("deskCode"));
     	
     	//  user id查user
     	String userIdOrName = CasinoWebUtil.getCurrentUserId();
@@ -81,6 +78,14 @@ public class OrderController {
     		log.warn("[下注] user查无资料, id: {}", userIdOrName);
             return ResponseUtil.custom("玩家资料错误");
     	}
+    	
+    	// 桌台资料
+    	JSONObject deskJson = deskBusiness.queryDeskById(betVO.getTableId());
+    	if ( deskJson==null ) {
+    		log.warn("[下注] 桌台号查无资料, table id: {}", betVO.getTableId());
+            return ResponseUtil.custom("桌台资料错误");
+    	}
+    	GameInfo gameInfo = gameInfoBusiness.getGameInfo(deskJson.getString("deskCode"));
     	
     	// 检核限红
     	GameTgGroupInfo groupInfo = gameInfo.getTgGroupInfo(betVO.getTgChatId());
@@ -94,7 +99,7 @@ public class OrderController {
     	}
 
     	//  呼叫
-    	//	会员管理-下分api
+    	//	会员管理 - 下分api
     	String withdrawResult = assetsBusiness.withdraw(userVO.getId(), betVO.getAmount());
     	if ( StringUtils.isNotBlank(withdrawResult) ) {
     		log.warn("[下注] 下分失败");
