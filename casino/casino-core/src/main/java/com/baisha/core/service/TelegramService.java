e9370d436f40e9e863c4abe9744ad3a3fa2cdc5f
yihui <yihui@gmail.com>
2022-06-30 15:43:11 +0700

    去掉不需要的代码

--- casino/casino-core/src/main/java/com/baisha/core/service/TelegramService.java
+++ casino/casino-core/src/main/java/com/baisha/core/service/TelegramService.java
@@ -1,16 +1,11 @@
 package com.baisha.core.service;
 
-import java.util.Map;
-
+import com.baisha.core.constants.RedisKeyConstants;
 import com.baisha.core.dto.SysTelegramDto;
+import com.baisha.modulespringcacheredis.util.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.baisha.core.constants.RedisKeyConstants;
-import com.baisha.core.vo.response.LimitStakesVO;
-import com.baisha.modulespringcacheredis.util.RedisUtil;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
@@ -36,20 +31,6 @@ public class TelegramService {
      */
     public void setSysTelegram(SysTelegramDto dto) {
         redisUtil.setValue(RedisKeyConstants.SYS_TELEGRAM, dto);
-    }
-
-    public LimitStakesVO getLimitStakes(String tgGroupdId) {
-        @SuppressWarnings("unchecked")
-        Map<String, Object> map = (Map<String, Object>) redisUtil.hget(RedisKeyConstants.GROUP_TELEGRAM_BOUND, tgGroupdId);
-        LimitStakesVO result = new LimitStakesVO();
-
-        if (map != null) {
-            result.setMinAmount((Integer) map.get("minAmount"));
-            result.setMaxAmount((Integer) map.get("maxAmount"));
-            result.setMaxShoeAmount((Integer) map.get("maxShoeAmount"));
-        }
-
-        return result;
     }
 
 }
