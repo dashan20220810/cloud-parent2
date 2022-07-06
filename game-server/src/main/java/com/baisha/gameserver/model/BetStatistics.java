ad8c91b9e460b1eaaf97277dbd33d8ae8425cb94
yihui <yihui@gmail.com>
2022-07-06 13:37:08 +0700

    默认值

--- game-server/src/main/java/com/baisha/gameserver/model/BetStatistics.java
+++ game-server/src/main/java/com/baisha/gameserver/model/BetStatistics.java
@@ -19,7 +19,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Entity
-@Table(name = "Bet_Statistics", indexes = {@Index(columnList = "user_id"),@Index(columnList = "statistics_date")})
+@Table(name = "Bet_Statistics", indexes = {@Index(columnList = "user_id"), @Index(columnList = "statistics_date")})
 @ApiModel(value = "GS-Bet对象", description = "注单")
 public class BetStatistics extends BaseEntity {
 
@@ -29,21 +29,21 @@ public class BetStatistics extends BaseEntity {
     @ApiModelProperty("user_id")
     @Column(name = "user_id", nullable = false)
     private Long userId;
-    
+
     @ApiModelProperty("统计日期")
     @Column(name = "statistics_date", nullable = false, columnDefinition = "int(11) COMMENT '统计日期 yyyyMMdd'")
     private Integer statisticsDate;
 
     @ApiModelProperty(value = "输赢金额")
     @Column(columnDefinition = "decimal(16,2) comment '输赢金额'")
-    private BigDecimal winAmount;
+    private BigDecimal winAmount = BigDecimal.ZERO;
 
     @ApiModelProperty(value = "流水")
     @Column(columnDefinition = "decimal(16,2) comment '流水'")
-    private BigDecimal flowAmount;
+    private BigDecimal flowAmount = BigDecimal.ZERO;
 
     @ApiModelProperty(value = "返水")
     @Column(columnDefinition = "decimal(16,2) comment '返水'")
-    private BigDecimal returnAmount;
-    
+    private BigDecimal returnAmount = BigDecimal.ZERO;
+
 }
