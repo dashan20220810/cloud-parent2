615bb555c34f4fbe0b460a72ebbdba6b0fa03fef
ggfugg777 <12345678>
2022-07-06 11:39:13 +0800

    删除无用的代码

--- telegram-bot-api/src/main/java/com/baisha/handle/TelegramMessageHandler.java
+++ telegram-bot-api/src/main/java/com/baisha/handle/TelegramMessageHandler.java
@@ -361,26 +361,4 @@ public class TelegramMessageHandler {
         }
         return result;
     }
-
-    /**
-     * Unicode轉 漢字字串
-     *
-     * @param str \u6728
-     * @return '木' 26408
-     */
-    public static String unicodeToString(String str) {
-        Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
-        Matcher matcher = pattern.matcher(str);
-        char ch;
-        while (matcher.find()) {
-            // group 6728
-            String group = matcher.group(2);
-            // ch:'木' 26408
-            ch = (char) Integer.parseInt(group, 16);
-            // group1 \u6728
-            String group1 = matcher.group(1);
-            str = str.replace(group1, ch + "");
-        }
-        return str;
-    }
 }
