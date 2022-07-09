9e9a2a471ff6fd9bc7baa5be7a9040a08ebec67c
xiaozhi <12345678>
2022-07-09 19:54:10 +0700

    开局，开牌

---
+++ module-common/src/main/java/com/baisha/modulecommon/vo/mq/SettleFinishVO.java
@@ -0,0 +1,31 @@
+package com.baisha.modulecommon.vo.mq;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 结算完成
+ * @author yihui
+ */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SettleFinishVO implements Serializable {
+
+    /**
+     * 局号
+     */
+    private String noActive;
+
+    /**
+     * 开牌结果
+     */
+    private String openCardResult;
+
+
+}
