9a248eb3e99c1c64bb2ecd5bb5cc0b243d25c612
xiaozhi <12345678>
2022-07-25 10:43:05 +0700

    修改结算金额

--- casino/casino-web/src/main/java/com/baisha/casinoweb/service/AsyncCommandService.java
+++ casino/casino-web/src/main/java/com/baisha/casinoweb/service/AsyncCommandService.java
@@ -384,6 +384,9 @@ public class AsyncCommandService {
 				
 				betHistoryList = betHistoryList.stream().sorted(Comparator
 						.comparingDouble(bet -> (-bet.getWinAmount()))).collect(Collectors.toList());
+				betHistoryList.forEach(obj -> {
+					obj.setWinStrAmount(BigDecimal.valueOf(obj.getWinAmount()).stripTrailingZeros().toPlainString());
+				});
 				top20WinUsers.put(tgGroupId, betHistoryList.subList(0, Math.min(betHistoryList.size(), 20)));
 			}
 		}
@@ -435,6 +438,7 @@ public class AsyncCommandService {
     	
     	private String username;
     	private Double winAmount;
+		private String winStrAmount;
     }
     
 }
